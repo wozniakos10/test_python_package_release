c56f5b9cddee6251827df4191507b51bd3020198
Dawid Woźniak <81079505+wozniakos10@users.noreply.github.com>
2026-05-30 13:16:43 +0100

    chore(main): release test-python-package-release 0.3.0

--- src/test_python_package_release/__init__.py
+++ src/test_python_package_release/__init__.py
@@ -1,6 +1,6 @@
 """Test Python package release — template library."""
 
-__version__ = "0.2.0"  # managed by release-please
+__version__ = "0.3.0"  # managed by release-please
 
 from test_python_package_release.calculator import add, divide, multiply, power, subtract
 from test_python_package_release.strings import reverse_string, snake_to_camel
